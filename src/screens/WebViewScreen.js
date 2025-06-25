923bb55a160fb5d495ef6b4e64c8564c03b15d90
rakib-mbstu <mirrakibulislam4@gmail.com>
2025-06-25 19:02:53 +0600

    feat: download feature added

--- src/screens/WebViewScreen.js
+++ src/screens/WebViewScreen.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { StyleSheet, Platform, Linking, Alert } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { StyleSheet } from 'react-native';
 
 const WebViewScreen = ({ route }) => {
     const { url } = route.params;
+
+    // Android: Handle file download
+    const handleFileDownload = ({ nativeEvent }) => {
+        Alert.alert(
+            'Download requested',
+            'Opening download in browser...',
+            [{ text: 'OK' }]
+        );
+        Linking.openURL(nativeEvent.downloadUrl);
+    };
+
+    // iOS: Intercept download links and open in Safari
+    const handleShouldStartLoadWithRequest = (event) => {
+        if (
+            Platform.OS === 'ios' &&
+            event.url !== url &&
+            /\.(pdf|zip|docx?|xlsx?|pptx?|mp3|mp4|apk|csv|txt)$/i.test(event.url)
+        ) {
+            Linking.openURL(event.url);
+            return false;
+        }
+        return true;
+    };
 
     return (
         <WebView
@@ -11,6 +34,8 @@ const WebViewScreen = ({ route }) => {
             source={{ uri: url }}
             javaScriptEnabled={true}
             domStorageEnabled={true}
+            onFileDownload={Platform.OS === 'android' ? handleFileDownload : undefined}
+            onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest}
         />
     );
 };
